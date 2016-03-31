42a3b9ebca892f647de835af13e94b835f059d5d
apple2015 <apple2015@apple2015demac-mini.local>
2016-03-31 17:07:29 +0800

    Initial Commit

---
+++ HealthKItDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HealthKItDemo
+//
+//  Created by apple2015 on 16/3/31.
+//  Copyright © 2016年 apple2015. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
